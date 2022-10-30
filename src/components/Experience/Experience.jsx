6ac242a0358f72d4c049b4f5934863a5854ff2eb
Muhammad Awab <muhammadawab82002@gmail.com>
2022-10-31 04:41:04 +0500

    change experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,7 +3,7 @@ import './Experience.css'
 import { BsFillPatchCheckFill } from 'react-icons/bs'
 const Experience = () => {
       return (
-            <section id='experience '>
+            <section id='experience'>
                   <h5>What Skills I Have</h5>
                   <h2>My Experience</h2>
                   <div className="d-flex justify-content-center align-items-center experience_container">
